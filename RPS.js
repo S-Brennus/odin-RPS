97c65b2c39b6eb2b4a7558e2635ddbe5b6ab784e
S-Brennus <101867356+S-Brennus@users.noreply.github.com>
2023-07-31 15:33:13 -0600

    Make interface

--- RPS.js
+++ RPS.js
@@ -1,19 +1,19 @@
 // function to generate computer option
 
 function getComputerChoice() {
-  let computerOption;
+  let computerChoice;
   let randomNumber = Math.floor(Math.random() * 3 + 1); //randomly generate a number from 1 to 3
     if (randomNumber === 1) { //assign generated number to a certain option - either rock, paper or scissors
-     computerOption = "Rock";
+     computerChoice = "rock";
     } else if (randomNumber === 2) {
-     computerOption = "Paper";
+     computerChoice = "paper";
     } else {
-     computerOption = "Scissors";
+     computerChoice = "scissors";
     }
-    return computerOption; //return the choice, can be stored in a variable later
+    return computerChoice; //return the choice, can be stored in a variable later
 }
  
-
+/*
 //prompt the user for input, if the user presses cancel - announce it and stop execution.
 
 function getUserChoice() {
@@ -27,89 +27,117 @@ function getUserChoice() {
 }
 
 
-//make sure the entered word is correct, otherwise keep prompting for input. If user presses cancel - quit the game
+//keep prompting if the word entered does not match rock, paper or scissors
 function checkIfValid() {
  while (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
   getUserChoice();
  }
  }
 
+*/
+
+let playerScore = { //object with players
+  user : 0,
+  ai : 0
+}
 
  function round(userChoice, computerChoice) {
   let info;
   if (userChoice === "rock" && computerChoice === "scissors") {
     info = "Rock beats scissors, you win!";
+    ++playerScore.user;
   } else if (userChoice === "scissors" && computerChoice === "paper") {
     info = "Scissors beat paper, you win!";
+    ++playerScore.user;
   } else if (userChoice === "paper" && computerChoice === "rock") {
     info = "Paper beats rock, you win!";
+    ++playerScore.user;
   } else if (userChoice === "rock" && computerChoice === "paper") {
     info = "Rock loses to paper, you lose!";
+    ++playerScore.ai;
   } else if (userChoice === "scissors" && computerChoice === "rock") {
     info = "Scissors lose to rock, you lose!";
+    ++playerScore.ai;
   } else if (userChoice === "paper" && computerChoice === "scissors") {
     info = "Paper loses to scissors, you lose!"; 
+    ++playerScore.ai;
   } else { 
     info = "It's a tie!"
   }
   return info;
 }
-
+/*
 let computerChoice;
 let userChoice;
+*/
 
-let playerScore = { //could have done it with just regular values, no object creation was necessary
-  user : 0,
-  ai : 0
-}
-
-function selectWinner() { //calculate score after the round is over
- 
-  if (round(userChoice, computerChoice).match("win")) {
-    playerScore.user++;
-  } else if (round(userChoice, computerChoice).match("lose")) {
-    playerScore.ai++;
-  }
-  return;
-}
-
-// the main which includes all functions together. Loop for as long as it takes, until one side is a winner 3 times
+/*
+// the main loop, which includes all functions together. Loop for as long as it takes, until one side wins 3 times
 while (playerScore.user !== 3 && playerScore.ai !== 3) {
-  computerChoice = getComputerChoice().toLowerCase();
+  computerChoice = getComputerChoice();
   userChoice = getUserChoice().toLowerCase();
   checkIfValid();
   alert(round(userChoice, computerChoice));
   selectWinner();
 } 
 
+*/ 
+
 //score and winner calculation and announcement
-alert (`Your score is ${playerScore.user}, computer's score is ${playerScore.ai}`);
-if (playerScore.user === 3) {
-  alert("Victory!")
-  } else if (playerScore.ai === 3) {
-    alert("Defeat!")
-  } else {
-    alert("It's a tie! Play again!")     
+
+
+
+
+
+  const mainDiv = document.querySelector('.main');
+  const winnerAnnounce = document.createElement('div');
+  winnerAnnounce.style.cssText = 'text-align:center; color: white;'
+  winnerAnnounce.classList.add('winner-announce');
+  winnerAnnounce.textContent = "Press any button to play!";
+  mainDiv.appendChild(winnerAnnounce);
+
+  const score = document.createElement('div')
+  score.classList.add('score');
+  score.style.cssText = 'text-align:center; color: white;'
+  score.textContent = 'Your score will be displayed here';
+  mainDiv.appendChild(score);
+  
+  function roundWinner() {
+    winnerAnnounce.textContent = round(userChoice, computerChoice);
+    score.textContent = `Your score: ${playerScore.user}, AI score: ${playerScore.ai}`;
   }
+  function gameResult() {
+    if (playerScore.user === 3) {
+      alert("You have won the game");
+      location.reload();
+    } else if (playerScore.ai === 3) {
+      alert("Machines have taken over the world!");
+      location.reload();
+    }
+  }
+let userChoice;
+let computerChoice;
 
+const rockBtn = document.querySelector('.rock');
+rockBtn.addEventListener('click', () => {
+  userChoice = "rock";
+  computerChoice = getComputerChoice();
+  roundWinner();
+  gameResult();
+  });
 
+const paperBtn = document.querySelector('.paper');
+paperBtn.addEventListener('click', () => {
+  userChoice = "paper";
+  computerChoice = getComputerChoice();
+  roundWinner();
+  gameResult();
+});
 
-
-
-
-
-/*
-
-  localeCompare messing around
-
- //example
-const areEqual = string1.localeCompare(string2, undefined, {
-  sensitivity: 'base' })
-
-const comparisonResult = testOption.localeCompare(myOption, undefined, { sensitivity: 'base' });
-While (comparisonResult !== 0) {
-  prompt("You are a pirate!");
-}
-console.log(comparisonResult);
-
-*/
+const scissorsBtn = document.querySelector('.scissors');
+scissorsBtn.addEventListener('click', () => {
+  userChoice = "scissors";
+  computerChoice = getComputerChoice();
+  roundWinner();
+  gameResult();
+});
